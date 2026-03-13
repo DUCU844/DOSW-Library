da961db2c8e0d6b843dd0923772168482a2bdba0
DUCU844 <adrianducuara844@gmail.com>
2026-03-13 13:31:02 -0500

    feat: Organzación y creación de carpetas correcta en laboratorio

---
+++ src/main/java/edu/eci/dosw/DOSW_Library/core/exception/LoanNotAvailableException.java
@@ -0,0 +1,7 @@
+package edu.eci.dosw.DOSW_Library.core.exception;
+
+public class LoanNotAvailableException extends RuntimeException {
+    public LoanNotAvailableException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/edu/eci/dosw/DOSW_Library/core/exception/UserNotAvailableException.java
@@ -0,0 +1,7 @@
+package edu.eci.dosw.DOSW_Library.core.exception;
+
+public class UserNotAvailableException extends RuntimeException {
+    public UserNotAvailableException(String message) {
+        super(message);
+    }
+}
